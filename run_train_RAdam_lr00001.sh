cb6357f730724736f4c91f0eddd82af530683990
timonmerk <timon.merk95@gmail.com>
2024-12-05 16:29:48 +0100

    fix typo

--- run_train_RAdam_lr00001.sh
+++ run_train_RAdam_lr00001.sh
@@ -5,4 +5,4 @@
 #SBATCH -o logs/RAdam_out_lr.out
 #SBATCH -e logs/RAdam_lr.err
 
-uv run src/main.py --name lr_00001 --lr 0.00001 --batch_size 1024 --optimizer RAadam --masking_ratio 0.1 --mean_mask_length 10
+uv run src/main.py --name lr_00001 --lr 0.00001 --batch_size 1024 --optimizer RAdam --masking_ratio 0.1 --mean_mask_length 10
