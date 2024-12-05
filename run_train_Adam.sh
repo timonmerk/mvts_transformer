7c6b61cb534a3e288d9130177b26adfd130c5b33
timonmerk <timon.merk95@gmail.com>
2024-12-05 16:22:22 +0100

    fix typo

--- run_train_Adam.sh
+++ run_train_Adam.sh
@@ -2,9 +2,6 @@
 #SBATCH --mem=100GB
 #SBATCH --gres=gpu:tesla:3
 #SBATCH --time=7-0
-# Extract the base name of the script file (without extension)
-SCRIPT_NAME=$(basename "$0" .sh)
-#SBATCH -o logs/${SCRIPT_NAME}.out
-#SBATCH -e logs/${SCRIPT_NAME}.err
-
-uv run src/main.py --name lr_0001 --lr 0.0001 --batch_size 1024 --optimizer Aadam --masking_ratio 0.1 --mean_mask_length 10
+#SBATCH -o logs/Adam_out.out
+#SBATCH -e logs/Adam.err
+uv run src/main.py --name Adam --lr 0.0001 --batch_size 1024 --optimizer Adam --masking_ratio 0.1 --mean_mask_length 10
--- run_train_RAdam.sh
+++ run_train_RAdam.sh
@@ -2,9 +2,7 @@
 #SBATCH --mem=100GB
 #SBATCH --gres=gpu:tesla:3
 #SBATCH --time=7-0
-# Extract the base name of the script file (without extension)
-SCRIPT_NAME=$(basename "$0" .sh)
-#SBATCH -o logs/${SCRIPT_NAME}.out
-#SBATCH -e logs/${SCRIPT_NAME}.err
+#SBATCH -o logs/RAdam_out.out
+#SBATCH -e logs/RAdam.err
 
-uv run src/main.py --name lr_0001 --lr 0.0001 --batch_size 1024 --optimizer RAadam --masking_ratio 0.1 --mean_mask_length 10
+uv run src/main.py --name RAdam --lr 0.0001 --batch_size 1024 --optimizer RAadam --masking_ratio 0.1 --mean_mask_length 10
