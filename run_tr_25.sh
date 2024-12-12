c118f6c4caf138d024b400ee956c12f46a21e609
timonmerk <timon.merk95@gmail.com>
2024-12-12 21:12:19 +0100

    fix typo

--- run_tr_25.sh
+++ run_tr_25.sh
@@ -5,4 +5,4 @@
 #SBATCH --cpus-per-task=4
 #SBATCH -o logs/25.out
 #SBATCH -e logs/25.err
-uv run src/main.py --name RAdam25--lr 0.00001 --batch_size 2056 --optimizer RAdam --masking_ratio 0.1 --mean_mask_length 10 --mask_mode separate --num_workers 3 --max_seq_len 25
+uv run src/main.py --name RAdam25 --lr 0.00001 --batch_size 2056 --optimizer RAdam --masking_ratio 0.1 --mean_mask_length 10 --mask_mode separate --num_workers 3 --max_seq_len 25
