7d2a2fbcc824270da2a28159382f18db7ec32657
timonmerk <timon.merk95@gmail.com>
2024-12-21 13:53:14 +0100

    fix typo

--- run_tr_250_WAdam_MAE_lre5.sh
+++ run_tr_250_WAdam_MAE_lre5.sh
@@ -5,4 +5,4 @@
 #SBATCH --cpus-per-task=4
 #SBATCH -o logs/W250AdamMAEe4.out
 #SBATCH -e logs/W250AdamMAEe4.err
-uv run src/main.py --name WAdam250 --lr 0.0001 --batch_size 1024 --optimizer WAdam --masking_ratio 0.4 --mean_mask_length 50 --mask_mode concurrent --num_workers 3 --max_seq_len 250 --num_heads 8 --d_model 256 --dim_feedforward 512 --num_layers 6 --epochs 200 --loss MAE
+uv run src/main.py --name WAdam250 --lr 0.0001 --batch_size 1024 --optimizer AdamW --masking_ratio 0.4 --mean_mask_length 50 --mask_mode concurrent --num_workers 3 --max_seq_len 250 --num_heads 8 --d_model 256 --dim_feedforward 512 --num_layers 6 --epochs 200 --loss MAE
