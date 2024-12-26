7fa218b5b3036d71d77470dd595d7032c53dcc99
timonmerk <timon.merk95@gmail.com>
2024-12-26 13:59:16 +0100

    fix mask ratio

--- run_tr_250_WAdam_MSE_lre4.sh
+++ run_tr_250_WAdam_MSE_lre4.sh
@@ -5,4 +5,5 @@
 #SBATCH --cpus-per-task=4
 #SBATCH -o logs/W250AdamMSEe4.out
 #SBATCH -e logs/W250AdamMSEe4.err
-uv run src/main.py --name WAdam250 --lr 0.0001 --batch_size 1024 --optimizer AdamW --masking_ratio 0.4 --mean_mask_length 50 --mask_mode concurrent --num_workers 3 --max_seq_len 250 --num_heads 8 --d_model 256 --dim_feedforward 512 --num_layers 6 --epochs 200 --loss MSE --concat_fft
+uv run src/main.py --name WAdam250Ratio02 --lr 0.0001 --batch_size 1024 --optimizer
+AdamW --masking_ratio 0.2 --mean_mask_length 10 --mask_mode concurrent --num_workers 3 --max_seq_len 250 --num_heads 8 --d_model 256 --dim_feedforward 512 --num_layers 6 --epochs 200 --loss MSE --concat_fft
